71b3f12bd702d07e545090daf6c1ddfc493f472c
Anna-Ivanova <aivanova0519@gmail.com>
2016-01-10 20:34:26 +0200

    sec commit

---
+++ Homework15.java
@@ -0,0 +1,9 @@
+class Homework15{
+public static void main (String[]args){
+int N=368;
+int min=N/60;
+int sec=N%60;
+System.out.println("min="+ min); 
+System.out.println("sec="+ sec); 
+}
+}
---
+++ Homework16.java
@@ -0,0 +1,8 @@
+class Homework16{
+public static void main (String[]args){
+int K=15;
+int N=2;
+int DEN=(N+K)%7;
+System.out.println("DEN NEDELI="+ DEN); 
+}
+}
