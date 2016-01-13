f7fbdfebec6e0db492c6a7e773109ce3c5758c5e
Anna-Ivanova <aivanova0519@gmail.com>
2016-01-13 23:03:52 +0200

    9 zadacha

---
+++ Homework9.java
@@ -0,0 +1,9 @@
+class Homework9{
+public static void main (String[]args){
+int RAS=Integer.parseInt(args[0]);
+double MILE=62.14;
+double GAL=3.875;
+double PEREVOD=MILE/GAL;
+System.out.println("RASHOD MILE/GALLON= "+PEREVOD); 
+}
+}
