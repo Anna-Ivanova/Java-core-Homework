be227c456907fef45c4e09c2ef22121c03f3af4b
Anna-Ivanova <aivanova0519@gmail.com>
2016-01-17 19:58:00 +0200

    add new

---
+++ Homework17.java
@@ -0,0 +1,8 @@
+class Homework17{
+public static void main (String[]args){
+int a=Integer.parseInt(args[0]);
+if (a>99 & a<1000 & a%2==1){
+	System.out.print (a+" trehznachnoe nechetnoe chislo");
+}
+}
+}
