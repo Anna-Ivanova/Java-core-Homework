313853ef8e414a61cf22fc10ec2eda34b370716b
Anna-Ivanova <aivanova0519@gmail.com>
2016-01-26 08:18:23 +0200

    ispravila kod soglasno zamechaniyam

--- Homework58.java
+++ Homework58.java
@@ -2,14 +2,9 @@ class Homework58{
 public static void main(String[]args){
 int zpchas=Integer.parseInt(args[0]);
 int chasy=Integer.parseInt(args[1]);	
-if (zpchas<8){
-System.out.println ("pls input salary not less than 8");
-}
-if (chasy>60){
-	System.out.println ("your employer dont have to work more than 60 hours");
-}
 double zpweek;
-if (chasy>40){
+if ((zpchas>=8)&(chasy<60)){
+	if (chasy>40){
 	zpweek=(zpchas*40)+(zpchas*(chasy-40)*1.5);
 	System.out.println("Salary for week= "+zpweek);
 	
@@ -18,4 +13,13 @@ if (chasy>40){
 		System.out.println("Salary for week= "+zpweek);
 }
 }
-} 
+else if (chasy>60){
+System.out.println ("your employer dont have to work more than 60 hours");
+}
+else if (zpchas<8){
+	System.out.println ("pls input salary not less than 8");
+}	
+}
+	
+}	
+	
