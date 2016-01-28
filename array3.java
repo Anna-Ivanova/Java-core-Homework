1d6b531a9235b6bf4ba40135711c4464c4850bbe
Anna-Ivanova <aivanova0519@gmail.com>
2016-01-28 08:17:08 +0200

    revers

---
+++ array3.java
@@ -0,0 +1,16 @@
+class array3{ 
+public static void main (String[]args){
+
+int[] array = { 50, 60, 30, 20, 10};
+
+    for(int i=0; i<array.length;i++){	
+    	System.out.print(array[i]+" ");}
+    	System.out.println(" ");
+   
+    for(int i=array.length-1; i>=0;i--){
+    
+    System.out.print(array[i]+" ");
+    }
+    	
+}
+}
