1163f902e65dbe8a9ea02f94e2513fcfbc80d16d
Anna-Ivanova <aivanova0519@gmail.com>
2016-01-29 01:24:45 +0200

    corrected

--- array6.java
+++ array6.java
@@ -2,23 +2,23 @@ class array6{
  public static void main (String[]args){
 
     int n=Integer.parseInt(args[0]);  
-	int[] array = { 50, 60, 30, 20, 10};
+	      
+    int[] array = { 50, 60, 30, 20, 10};
     
-    for(int i=0; i<array.length;i++){	
+       for(int i=0; i<array.length;i++){	
     	System.out.print(array[i]+" ");}
     	System.out.println(" ");
    
-    for(int i=0; i<(array.length-n);i++){
+       for(int i=0; i<(array.length-n);i++){
     	array[i]=array[i+n];
-    	
     }
-    for(int i=n+1; i<array.length;i++){
+    	
+        for(int i=array.length-n; i<array.length;i++){
     	
 	    array[i] = 0;
     }
-    for(int i=0;i<array.length;i++){
+       for(int i=0;i<array.length;i++){
     	System.out.print(array[i]+" ");
     	}
 }
 }
-
