e7bdb9070c9a50db914ea54ddbbd7d2998d96818
Anna-Ivanova <aivanova0519@gmail.com>
2016-02-09 22:02:32 +0200

    update

--- dnk.java
+++ dnk.java
@@ -1,7 +1,7 @@
 class dnk{
 public static void main (String[]args){
 String[] dnk = new String [10];
-dnk[0]="GJCG";
+dnk[0]="GJCASERRRTTTTG";
 dnk[1]="AAVVGDR";
 dnk[2]="CHJKLGG";
 dnk[3]="GCOHGCBNCCCGGGG";
@@ -12,31 +12,39 @@ dnk[7]="GCOHGCBNCGG";
 dnk[8]="GCOHGCBNNNNFFDDDSSCCGGGG";
 dnk[9]="GCOHGCBNCCCGGGG";
 
+int[]rez=new int[10];
+int index=0;
 
     
-               for(String text:dnk){ 
-                      gccount(text);
-					   int c=gccount(text);
-					
-					  System.out.print(c +" ");
-					 }
-					
-} 
+               for(int i=0;i<dnk.length;i++){ 
+                       String text=dnk[i];
+                          rez[i]=gccount(text);
+                          
+                          if(rez[index]<rez[i]){
+                            index=i;
+                          }
+                       int rezult=rez[index];
+                                                          
+                                          }
+               System.out.print("The highes GC contains in index"+" "+index+" ");
+                                         
+                                        }
+                                       
  
-	public static int gccount(String tex){
-		       int count=0;
+       public static int gccount(String tex){
+                      int count=0;
                char g='G';
                char c='C';
-			   int procent=0;
-		for (int j=0;j<tex.length();j++){
+                          int procent=0;
+               for (int j=0;j<tex.length();j++){
                        char ch=tex.charAt(j);
                        if((ch==g)||(ch==c)){
                         count++;
-	}
-	            int len=tex.length();
-					  
-	            procent=(count*100)/len;
-	}
-	 return procent;
-	}
+       }
+                   int len=tex.length();
+                                         
+                   procent=(count*100)/len;
+       }
+        return procent;
+       }
 }
