63eb53a8f691b476647dc379b2b74dbae613c6cc
Anna-Ivanova <aivanova0519@gmail.com>
2016-02-09 06:59:51 +0200

    dnk

--- dnk.java
+++ dnk.java
@@ -1,18 +1,24 @@
 class dnk{
 public static void main (String[]args){
-String[] dnk = new String [4];
+String[] dnk = new String [10];
 dnk[0]="GJCG";
 dnk[1]="AAVVGDR";
 dnk[2]="CHJKLGG";
 dnk[3]="GCOHGCBNCCCGGGG";
+dnk[4]="GCOHGCBNCJJGBGGGGDCCGGGG";
+dnk[5]="GCCGCGCGCGCGCGCGCGCGCGKDOOEUEUEUOHGCBNCCCGGGG";
+dnk[6]="GKKKKKKKAAAAAJJRJTJTJTJTJTJTJTJLL;CCCCCCCOHGCBNCCCGGGG";
+dnk[7]="GCOHGCBNCGG";
+dnk[8]="GCOHGCBNNNNFFDDDSSCCGGGG";
+dnk[9]="GCOHGCBNCCCGGGG";
+
 
     
                for(String text:dnk){ 
                       gccount(text);
-					  int len=text.length();
-					  int c=gccount(text);
-					  int procent=(c*100)/len;
-					   System.out.print(procent+" ");  
+					   int c=gccount(text);
+					
+					  System.out.print(c +" ");
 					 }
 					
 } 
@@ -21,13 +27,16 @@ dnk[3]="GCOHGCBNCCCGGGG";
 		       int count=0;
                char g='G';
                char c='C';
+			   int procent=0;
 		for (int j=0;j<tex.length();j++){
                        char ch=tex.charAt(j);
                        if((ch==g)||(ch==c)){
                         count++;
 	}
+	            int len=tex.length();
+					  
+	            procent=(count*100)/len;
 	}
-	 return count;
+	 return procent;
 	}
 }
-
